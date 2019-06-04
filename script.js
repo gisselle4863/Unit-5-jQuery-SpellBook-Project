2f120e5a424391124ec8a61b1ae495b6fa9a4452
gisselle4863 <51371571+gisselle4863@users.noreply.github.com>
2019-06-04 09:37:24 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,35 @@
+//Spell #1
+
+$(".possession").click(function(){
+ $(".possession").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$("      ").click(function(){
+ 
+});
+
+
+
+//Spell #3
+
+$("      ").click(function(){
+ 
+});
+$(".garage-door-opener").click(function() {
+    $(".garage-door").slideToggle();
+});
+
+$(".pokeball").click(function() {
+    $(".pikachu").fadeToggle();
+});
+
+$(".light-switch").click(function() {
+        $(".light-bulb").toggle();
+});
+
+
+
